ac4cbd3c2466bfb0fcfcc7c80729bc1a1c2fb452
marekch96 <marek.checinski96@gmail.com>
2023-08-09 10:14:58 +0100

    bootsrap+flask render test

--- helmet_motorcycle_model_train.py
+++ helmet_motorcycle_model_train.py
@@ -3,8 +3,12 @@ import cv2
 
 
 # Load a model
-model = YOLO('yolov8l.yaml')  # build a new model from scratch most powerful yolo 
+model = YOLO('yolov8x.yaml')  # build a new model from scratch most powerful yolo 
 
 
 # Use the model
-results = model.train(data='config.yaml',epochs=50,batch=8,name='helmet_motorbike_custom_modelv8l_50e')
+results = model.train(data='config.yaml',epochs=50,batch=8,name='big_yolov8x_50e') #batch size incresed from 8 to 16  + 50 epochs #max vram that rtx 4070 can handle 
+
+
+#yolo task=detect mode=predict model=helmet_motorcycle_detection_best.pt source='input/video_3.mp4' show=True
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return f('test')
+    return render_template('/templates/index.html')
 
 if __name__ == "__main__":
     app.run()
